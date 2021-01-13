46334e86c00b5ce9a9cab715e53fcda1d99fad08
chadmondwu <chadmondwu@gmail.com>
2021-01-13 14:22:44 -0800

    Push home page changes

--- docs/conf.py
+++ docs/conf.py
@@ -38,13 +38,10 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# The master toctree document.
-master_doc = 'index-copy.rst'
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.vscode']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -58,7 +55,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-#html_logo = 'py4web_art5.png'
+#html_logo
 html_theme_options = {
     'logo_only': True,
     'display_version': True,
